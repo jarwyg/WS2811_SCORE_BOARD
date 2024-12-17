1d085470133632ab4d2d1c985fe8da3532b022a5
jarwyg <user@localhost>
2024-12-18 00:35:05 +0100

    Added Release folder

---
+++ firmware/Transmitter/01_Score_Board_Matrix_Tx/Release/nrf24l01/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../nrf24l01/nrf24l01.c 
+
+OBJS += \
+./nrf24l01/nrf24l01.o 
+
+C_DEPS += \
+./nrf24l01/nrf24l01.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+nrf24l01/%.o: ../nrf24l01/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega328p -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
